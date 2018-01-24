4307628a8c8ab713b3d55a83114283fae62472e6
Marcin Zamelski <marcin.zamelski@funclub.pl>
2018-01-24 19:16:16 +0100

    Migrate to react 16. Some cleanups and tweaks.

--- src/index.js
+++ src/index.js
@@ -1,169 +1,172 @@
-import React, {PropTypes} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 const FPS = 20;
-const STEP = 1;
 const TIMEOUT = 1 / FPS * 1000;
 
-const Marquee = React.createClass({
-  propTypes: {
-    text: PropTypes.string,
+class Marquee extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+
+        this.state = {
+            animatedWidth: 0,
+            overflowWidth: 0,
+        }
+    }
+
+    componentDidMount() {
+        this._measureText();
+
+        if (this.props.hoverToStop) {
+            this._startAnimation();
+        }
+    }
+
+    componentDidUpdate() {
+        this._measureText();
+
+        if (this.props.hoverToStop) {
+            this._startAnimation();
+        }
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this._marqueeTimer);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.text.length !== nextProps.text.length) {
+            clearTimeout(this._marqueeTimer);
+            this.setState({
+                animatedWidth: 0
+            });
+        }
+    }
+
+    handleMouseEnter() {
+        if (this.props.hoverToStop) {
+            clearTimeout(this._marqueeTimer);
+        }
+        else if (this.state.overflowWidth > 0) {
+            this._startAnimation();
+        }
+    }
+
+    handleMouseLeave() {
+        if (this.props.hoverToStop && this.state.overflowWidth > 0) {
+            this._startAnimation();
+        }
+        else {
+            clearTimeout(this._marqueeTimer);
+            this.setState({
+                animatedWidth: 0
+            });
+        }
+    }
+
+    render() {
+        const style = {
+            position: 'relative',
+            display: 'inline-block',
+            transform: `translate3d(-${this.state.animatedWidth}px, 0px, 0px)`,
+            whiteSpace: 'nowrap'
+        };
+
+        return (
+            <div className={cx('ui-marquee', this.props.className)} style={{overflow: 'hidden'}}
+                 onMouseEnter={this.state.overflowWidth < 0 ? this.handleMouseEnter : null}
+                 onMouseLeave={this.state.overflowWidth < 0 ? this.handleMouseLeave : null}
+                 ref={(div) => {this.containerDiv = div}}
+            >
+                <span ref={(span) => { this.textSpan = span;}} style={style}>{this.props.text}</span>
+            </div>
+        );
+    }
+
+    _startAnimation() {
+        clearTimeout(this._marqueeTimer);
+        const isLeading = this.state.animatedWidth === 0;
+        const timeout = isLeading ? this.props.leading : TIMEOUT;
+
+        const animate = () => {
+            const {overflowWidth} = this.state;
+            let animatedWidth = this.state.animatedWidth + this.props.step;
+            const isRoundOver = animatedWidth > overflowWidth;
+
+            if (isRoundOver) {
+                if (this.props.loop) {
+                    animatedWidth = 0;
+                }
+                else {
+                    return;
+                }
+            }
+
+            if (isRoundOver && this.props.trailing) {
+                this._marqueeTimer = setTimeout(() => {
+                    this.setState({
+                        animatedWidth
+                    });
+
+                    this._marqueeTimer = setTimeout(animate, TIMEOUT);
+                }, this.props.trailing);
+            }
+            else {
+                this.setState({
+                    animatedWidth
+                });
+
+                this._marqueeTimer = setTimeout(animate, TIMEOUT);
+            }
+        };
+
+        this._marqueeTimer = setTimeout(animate, timeout);
+    }
+
+    _measureText() {
+        const {forceAnimation} = this.props;
+        if (this.containerDiv && this.textSpan) {
+            const containerWidth = this.containerDiv.offsetWidth;
+            const textWidth = this.textSpan.offsetWidth;
+            const overflowWidth =
+                forceAnimation ?
+                (textWidth - containerWidth) > 0 ? (textWidth - containerWidth) : containerWidth
+                               :
+                Math.max(textWidth - containerWidth, 0);
+
+            if (overflowWidth !== this.state.overflowWidth) {
+                this.setState({
+                    overflowWidth
+                });
+            }
+        }
+    }
+}
+
+
+Marquee.propTypes = {
+    text: PropTypes.node,
     hoverToStop: PropTypes.bool,
     loop: PropTypes.bool,
     leading: PropTypes.number,
     trailing: PropTypes.number,
-    className: PropTypes.string
-  },
+    className: PropTypes.string,
+    step: PropTypes.number,
+    forceAnimation: PropTypes.bool,
+};
 
-  getDefaultProps() {
-    return {
-      text: '',
-      hoverToStop: false,
-      loop: false,
-      leading: 0,
-      trailing: 0
-    };
-  },
-
-  getInitialState() {
-    return {
-      animatedWidth: 0,
-      overflowWidth: 0
-    };
-  },
-
-  componentDidMount() {
-    this._measureText();
-
-    if (this.props.hoverToStop) {
-      this._startAnimation();
-    }
-  },
-
-  componentDidUpdate() {
-    this._measureText();
-
-    if (this.props.hoverToStop) {
-      this._startAnimation();
-    }
-  },
-
-  componentWillUnmount() {
-    clearTimeout(this._marqueeTimer);
-  },
-
-  componentWillReceiveProps(nextProps) {
-          if(this.props.text.length != nextProps.text.length)
-          {
-              clearTimeout(this._marqueeTimer);
-              this.setState({
-                animatedWidth: 0
-              });
-          }
-  },
-
-  handleMouseEnter() {
-    if (this.props.hoverToStop) {
-      clearTimeout(this._marqueeTimer);
-    }
-    else if (this.state.overflowWidth > 0){
-      this._startAnimation();
-    }
-  },
-
-  handleMouseLeave() {
-    if (this.props.hoverToStop && this.state.overflowWidth > 0) {
-      this._startAnimation();
-    }
-    else {
-      clearTimeout(this._marqueeTimer);
-      this.setState({
-        animatedWidth: 0
-      });
-    }
-  },
-
-  render() {
-    const style = {
-      'position': 'relative',
-      'right': this.state.animatedWidth,
-      'whiteSpace': 'nowrap'
-    };
-
-    if (this.state.overflowWidth < 0) {
-      return (
-        <div className={`ui-marquee ${this.props.className}`} style={{overflow: 'hidden'}}>
-          <span ref="text" style={style} title={this.props.text}>{this.props.text}</span>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div className={`ui-marquee ${this.props.className}`} style={{overflow: 'hidden'}}
-             onMouseEnter={this.handleMouseEnter}
-             onMouseLeave={this.handleMouseLeave}>
-          <span ref="text" style={style} title={this.props.text}>{this.props.text}</span>
-        </div>
-      );
-    }
-  },
-
-  _startAnimation() {
-    clearTimeout(this._marqueeTimer);
-    const isLeading = this.state.animatedWidth === 0;
-    const timeout = isLeading ? this.props.leading : TIMEOUT;
-
-    const animate = () => {
-      const {overflowWidth} = this.state;
-      let animatedWidth = this.state.animatedWidth + STEP;
-      const isRoundOver = animatedWidth > overflowWidth;
-
-      if (isRoundOver) {
-        if (this.props.loop) {
-          animatedWidth = 0;
-        }
-        else {
-          return;
-        }
-      }
-
-      if (isRoundOver && this.props.trailing) {
-        this._marqueeTimer = setTimeout(() => {
-          this.setState({
-            animatedWidth
-          });
-
-          this._marqueeTimer = setTimeout(animate, TIMEOUT);
-        }, this.props.trailing);
-      }
-      else {
-        this.setState({
-          animatedWidth
-        });
-
-        this._marqueeTimer = setTimeout(animate, TIMEOUT);
-      }
-    };
-
-    this._marqueeTimer = setTimeout(animate, timeout);
-  },
-
-  _measureText() {
-    const container = ReactDOM.findDOMNode(this);
-    const node = ReactDOM.findDOMNode(this.refs.text);
-
-    if (container && node) {
-      const containerWidth = container.offsetWidth;
-      const textWidth = node.offsetWidth;
-      const overflowWidth = textWidth - containerWidth;
-
-      if (overflowWidth !== this.state.overflowWidth) {
-        this.setState({
-          overflowWidth
-        });
-      }
-    }
-  }
-});
+Marquee.defaultProps = {
+    text: '',
+    hoverToStop: false,
+    loop: false,
+    leading: 0,
+    trailing: 0,
+    step: 1,
+    forceAnimation: false,
+};
 
 module.exports = Marquee;
